d13e159cb20f2b2efe4e4c4752a1b2c18bbf1f33
luizzamprogno <lpzampronio@gmail.com>
2024-06-18 08:03:18 -0300

    Retirado prompt de downloads

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ def iniciar_driver():
 
 
     chrome_options.add_experimental_option('prefs', {
-        'download.prompt_for_download': False,
         'profile.default_content_setting_values.notifications': 2,
         'profile.default_content_setting_values.automatic_downloads':1,
     })
